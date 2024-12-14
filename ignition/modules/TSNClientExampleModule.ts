223d7ce8e3b3d39c86e7dd47063854b03d445990
Raffael Campos <raffael.rcampos@gmail.com>
2024-12-13 22:10:58 -0300

    refactor: remove TNConsumerExample contract and add TSNClientExampleModule
    - Deleted the TNConsumerExample contract, which was an example implementation for Chainlink Functions, to streamline the codebase.
    - Introduced a new TSNClientExampleModule for deploying the TNClientExample contract, enhancing modularity and organization.
    - The new module facilitates easier deployment and integration with the Chainlink Functions framework, providing a clearer structure for developers.

--- ignition/modules/TSNClientExampleModule.ts
+++ ignition/modules/TSNClientExampleModule.ts
@@ -1,17 +1,15 @@
-// SEPOLIA TSNConsumerExample#TNConsumerExample - 0xcfc6ec1b1D807BB16f0936257790fE6Aa52F2744
-
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { ROUTER_ADDRESS } from "../../constants";
 
 // npx hardhat ignition deploy ignition/modules/TSNConsumerExample.ts --network sepolia
 
-const TSNConsumerExampleModule = buildModule("TSNConsumerExample", (m) => {
+const TNClientExampleModule = buildModule("TNClientExample", (m) => {
   const _routerAddress = m.getParameter("_routerAddress", ROUTER_ADDRESS.sepolia);
-  const TNConsumerExample = m.contract("TNConsumerExample", [
+  const TNClientExample = m.contract("TNClientExample", [
       _routerAddress,
   ] );
 
-  return { TNConsumerExample  };
+  return { TNClientExample  };
 });
 
-export default TSNConsumerExampleModule;
+export default TNClientExampleModule;
